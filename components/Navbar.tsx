1dd765b511a7860781599f76e3b473254d0afd49
o3e45 <owen@owendobson.com>
2024-12-12 19:57:14 -0500

    modified:   components/Navbar.tsx owendobson.com inital \ baseline configuration

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,12 @@ export default function Navbar() {
   return (
     <header>
       <Link href="/">
-        <h1>Bear Blog Replacement Template</h1>
+        <h1>Owen Dobson</h1>
       </Link>
       <nav>
         <p>
           <Link href="/">Home</Link>
           <Link href="/blog/">Blog</Link>
-          <Link href="/the-grizzly-bear-chill-zone/">
-            The Grizzly Bear Chill Zone
           </Link>
         </p>
       </nav>
